99b472a2e5f15564ff5b90ba06cb15c5a24cead6
Drp310 <this.is.igor8@gmail.com>
2022-02-08 19:54:00 +0200

    ord commit

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,12 @@
 const {src,dest, watch } = require('gulp')
 const pug = require('gulp-pug')
 const del = require('del')
-
+const scss = require('gulp-sass')(require('sass'));
+function styling(){
+    return (src('style.scss'))
+    .pipe(scss())
+    .pipe(dest('style'))
+}
 function puging(){
     return(src('index.pug'))
     .pipe(pug())
@@ -12,3 +17,4 @@ function watching(){
 }
 exports.puging = puging
 exports.watching = watching
+exports.styling = styling
